706dd9efc8120f11c57c1fc427138afb9fe09912
Austin Sutherland <Austin Sutherland>
2017-08-01 23:37:02 +1200

    Better sizing on mobile

--- js/main.js
+++ js/main.js
@@ -3,6 +3,18 @@ $(document).ready(function() {
 
 })
 
+
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'PM' : 'AM';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime;
+}
+
 function weatherReport() {
 	
 	var lati = 56.817,
@@ -36,7 +48,7 @@ function weatherReport() {
 
 
     	for(var j = 0; j < 4; j++) {
-    		var time = new Date(forecast.hourly.data[j * 2].time * 1000),
+    		var time = formatAMPM(new Date(forecast.hourly.data[j * 2].time * 1000)),
     			chance = forecast.hourly.data[j].precipProbability * 100;
 
     		times[j][0] = time;
@@ -51,6 +63,11 @@ function weatherReport() {
 		$("#box2 .percent").html("" + times[1][1] + "%");
 		$("#box3 .percent").html("" + times[2][1] + "%");
 		$("#box4 .percent").html("" + times[3][1] + "%");
+
+		$("#box1 .time-title").html(times[0][0]);
+		$("#box2 .time-title").html(times[1][0]);
+		$("#box3 .time-title").html(times[2][0]);
+		$("#box4 .time-title").html(times[3][0]);
 
 
     });
